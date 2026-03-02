0110ad518f179b9e972bb902105f93555bfab620
qweq05293 <qweq05293@gmail.com>
2026-03-02 17:21:48 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { Title } from "./components/app-title";
+import { ModeToggle } from "./components/mode-toggle";
 
 export function App() {
-return <div>app</div>;
+    return (
+        <div className="p-4 w-85 flex flex-col gap-4 bg-radial-primary">
+            {/* Header */}
+            <div className="flex items-center justify-between">
+                <Title
+                    text={chrome.i18n.getMessage("extension_name")}
+                    align="left"
+                    size="lg"
+                />
+                <ModeToggle />
+            </div>
+        </div>
+    )
 }
 
 export default App;
