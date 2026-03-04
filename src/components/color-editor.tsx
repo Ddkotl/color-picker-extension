bcac85d90a6426b272d7c71746874774ee0635f8
qweq05293 <qweq05293@gmail.com>
2026-03-04 18:21:33 +0000

    add modal color palete

--- src/components/color-editor.tsx
+++ src/components/color-editor.tsx
@@ -1,19 +1,50 @@
 import { ColorPicker, useColor } from "react-color-palette";
 import "react-color-palette/dist/css/rcp.css";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Edit } from "lucide-react";
+import { Button } from "./ui/button";
 
 type Props = {
   initialColor: string;
   onChange: (color: string) => void;
+  onSave: (color: string) => void;
 };
 
-export function ColorEditor({ initialColor, onChange }: Props) {
+export function ColorEditor({ initialColor, onChange, onSave }: Props) {
   const [color, setColor] = useColor(initialColor);
 
-  return <ColorPicker
-    hideInput={["rgb", "hsv"]}
-    color={color}
-    onChange={(c) => {
-      setColor(c);
-      onChange(c.hex);
-    }} />
+  return (
+    <Sheet modal={true}>
+      <SheetTrigger className="flex gap-2">
+        <div
+          className="w-14 h-14 rounded-xl border shadow-inner cursor-pointer transition-transform hover:scale-105"
+          style={{ backgroundColor: color.hex }}
+        />
+        <span className="ml-2 text-sm text-foreground/90">
+          Edit Color </span>
+        <Edit />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit Color</SheetTitle>
+        </SheetHeader>
+        <SheetDescription>
+          <ColorPicker
+            hideInput={["rgb", "hsv"]}
+            color={color}
+            onChange={(c) => {
+              setColor(c);
+              onChange(c.hex);
+            }} />
+        </SheetDescription>
+        <SheetFooter>
+          <Button onClick={() => { onSave(color.hex) }}>Save</Button>
+          <SheetClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+
+  )
 }
